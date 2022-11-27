09b28f1f0469d9537f5f42f6a2606ab05e2d8cfb
SF <shforb@outlook.com>
2022-11-27 13:02:36 -0500

    Adds a group of checkboxes for classes

--- filter_window.py
+++ filter_window.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from spell_info import SpellInfo
 
 
 class SpellFilterWindow(tk.Toplevel):
@@ -14,9 +15,11 @@ class SpellFilterWindow(tk.Toplevel):
             self, text='Apply Filters', command=self.confirm_close
         )
         self.btn_cancel = ttk.Button(self, text='Cancel', command=self.dismiss)
+        self.frm_classes = ClassesFrame(self, relief=tk.GROOVE)
         # Placing the widgets on the grid
-        self.btn_confirm.grid(column=0, row=1)
-        self.btn_cancel.grid(column=1, row=1)
+        self.frm_classes.grid(column=0, row=0, columnspan=2, padx=5, pady=5)
+        self.btn_confirm.grid(column=4, row=5)
+        self.btn_cancel.grid(column=5, row=5)
 
     def dismiss(self):
         # Returning interactivity to the other windows
@@ -24,7 +27,35 @@ class SpellFilterWindow(tk.Toplevel):
         self.destroy()
 
     def confirm_close(self):
+        print(self.frm_classes.get_chk_values())
         self.dismiss()
+
+class ClassesFrame(ttk.Frame):
+    def __init__(self, parent, **keywords) -> None:
+        super().__init__(parent, **keywords)
+        self.parent = parent
+        self.add_widgets()
+
+    def add_widgets(self):
+        self.chk_variables_dict = {}
+        for id, spellcasting_class in enumerate(SpellInfo.classes):
+            label_key = spellcasting_class
+            chk_value = tk.IntVar(value=True)
+            chk_class = ttk.Checkbutton(
+                self, text=spellcasting_class, variable=chk_value
+            )
+            row_placement = id//2
+            col_placement = id%2
+            chk_class.grid(
+                row=row_placement, column=col_placement, padx=5, pady=5
+            )
+            self.chk_variables_dict[label_key] = chk_value
+
+    def get_chk_values(self) -> dict[str,bool]:
+        chk_values = {key: self.chk_variables_dict[key].get()
+            for key in self.chk_variables_dict
+        }
+        return chk_values
 
 
 if __name__ == "__main__":
