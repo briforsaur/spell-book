5adeb0e2bca9c24ce6e7a40687bf9f121f759289
SF <shforb@outlook.com>
2022-12-03 13:51:57 -0500

    Creates CheckboxGroup for any group of chkboxes

--- filter_window.py
+++ filter_window.py
@@ -15,7 +15,13 @@ class SpellFilterWindow(tk.Toplevel):
             self, text='Apply Filters', command=self.confirm_close
         )
         self.btn_cancel = ttk.Button(self, text='Cancel', command=self.dismiss)
-        self.frm_classes = ClassesFrame(self, relief=tk.GROOVE)
+        initial_class_chk_values = tuple(
+            [True for x in range(len(SpellInfo.classes))]
+        )
+        self.frm_classes = CheckboxGroup(
+            self, SpellInfo.classes, initial_class_chk_values, 2,
+            relief=tk.GROOVE
+        )
         # Placing the widgets on the grid
         self.frm_classes.grid(column=0, row=0, columnspan=2, padx=5, pady=5)
         self.btn_confirm.grid(column=4, row=5)
@@ -30,22 +36,24 @@ class SpellFilterWindow(tk.Toplevel):
         print(self.frm_classes.get_chk_values())
         self.dismiss()
 
-class ClassesFrame(ttk.Frame):
-    def __init__(self, parent, **keywords) -> None:
+class CheckboxGroup(ttk.Frame):
+    def __init__(self, parent, labels: tuple[str], initial_values: tuple[bool],
+            num_columns: int, **keywords) -> None:
         super().__init__(parent, **keywords)
         self.parent = parent
-        self.add_widgets()
+        self.add_widgets(labels, initial_values, num_columns)
 
-    def add_widgets(self):
+    def add_widgets(self, labels: tuple[str], initial_values: tuple[bool], 
+            num_columns: int):
         self.chk_variables_dict = {}
-        for id, spellcasting_class in enumerate(SpellInfo.classes):
-            label_key = spellcasting_class
-            chk_value = tk.IntVar(value=True)
+        for id, label in enumerate(labels):
+            label_key = label
+            chk_value = tk.IntVar(value=initial_values[id])
             chk_class = ttk.Checkbutton(
-                self, text=spellcasting_class, variable=chk_value
+                self, text=label, variable=chk_value
             )
-            row_placement = id//2
-            col_placement = id%2
+            row_placement = id//num_columns
+            col_placement = id%num_columns
             chk_class.grid(
                 row=row_placement, column=col_placement, padx=5, pady=5
             )
